f89075051b3f861f0d0857117604aca93538da82
AntonovaAlex <31790007+AntonovaAlex@users.noreply.github.com>
2020-07-23 14:39:07 +0300

    First commit
    Added Unity Project to GitHub

---
+++ Assets/RotationDNA.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationDNA : MonoBehaviour
+{
+	public int rotSpeed = 2;
+
+	void Update()
+	{
+		transform.Rotate(rotSpeed, 0, 0, Space.Self);
+	}
+}
---
+++ Assets/RotationObj.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObj : MonoBehaviour
+{
+	public int rotateSpeed = 2;
+
+    void Update()
+    {
+		transform.Rotate(0, rotateSpeed, 0, Space.Self);
+    }
+}
